2b650640f19bfe8db898f6fd24210335f40a04e6
Supriyo Paul <paulsupriyo64nk@gmail.com>
2024-08-24 23:08:53 +0530

    more basic test cases

--- src/test/java/com/example/makersharks/api/SupplierControllerTest.java
+++ src/test/java/com/example/makersharks/api/SupplierControllerTest.java
@@ -4,6 +4,7 @@ import com.example.makersharks.model.SCALE;
 import com.example.makersharks.model.SERVICE;
 import com.example.makersharks.model.Supplier;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
--- src/test/java/com/example/makersharks/dao/SupplierDaoTests.java
+++ src/test/java/com/example/makersharks/dao/SupplierDaoTests.java
@@ -3,27 +3,31 @@ package com.example.makersharks.dao;
 import com.example.makersharks.model.SCALE;
 import com.example.makersharks.model.SERVICE;
 import com.example.makersharks.model.Supplier;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class SupplierDaoTests {
 
-    private static SupplierDao supplierDao;
+    private SupplierDao supplierDao;
 
-    @BeforeAll
-    static void setup() {
+    @BeforeEach
+    void setup() {
+        supplierDao = new InMemorySupplierDataAccessService();
+    }
+
+    @AfterEach
+    void tearDown() {
         supplierDao = new InMemorySupplierDataAccessService();
     }
 
     @Test
-    public void add_supplier_should_success() {
-        Supplier supplier = new Supplier("Test", "www.test.com", "test", SCALE.small, SERVICE.casting);
-
-        supplierDao.addSupplier(supplier);
-
-        assertEquals(supplierDao.getSupplier().size(), 1);
+    public void get_suppliers_should_success() {
+        assertFalse(supplierDao.getSupplier().isEmpty());
     }
 }
